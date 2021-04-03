392f75e35c8b5183ecbdcd3757809b476e537dd9
Juan José Gazzola <juanjo_gazzola@hotmail.com>
2021-04-03 19:16:19 -0300

    Create install-docker-ec2.sh
    Install docker on an EC2 instance with Centos Linux 7.

---
+++ install-docker-ec2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# User Data instaling docker on Centos Linux 7 
+#Install docker
+sudo yum update
+sudo yum install -y yum-utils
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install -y docker-ce docker-ce-cli containerd.io
+sudo systemctl enable docker
+sudo usermod -a -G docker centos
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo reboot
+#Install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.28.6/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo reboot
